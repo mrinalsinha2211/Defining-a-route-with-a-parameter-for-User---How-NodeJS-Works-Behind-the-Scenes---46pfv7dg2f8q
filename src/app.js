e1667df8bf1d3c6f06427e6fa56d7a7964a4cc9d
mrinalsinha2211 <145749474+mrinalsinha2211@users.noreply.github.com>
2024-01-03 12:46:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('', (req, res) => {
+app.get("/user/:userId", (req, res) => {
   //Write a code here for endpoint /user/:userId and also print parameter in json form
+      const userId =req.params.userId;
+      res.status(200).json({userId});
 });
+
 
 module.exports = app;
